244e0ed7bdacef01bf474c5250ad410864fec2a2
sanju2062 <sk079997@gmail.com>
2024-08-22 22:28:34 +0530

    mongoURI passes directly

--- config/db.js
+++ config/db.js
@@ -1,26 +1,18 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+// const db = config.get("mongoURI");
 
 const connectDB = async () => {
-  // try {
-  //   await mongoose.connect(db, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true
-  //   });
-
-  //   console.log('MongoDB Connected...');
-  // } catch (err) {
-  //   console.error(err.message);
-  //   process.exit(1);
-  // }
   mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Increase timeout to 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    })
+    .connect(
+      "mongodb+srv://SanjuDeveloper:Mausam123@sanjudeveloper.rm0ud.mongodb.net/ABCD?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Increase timeout to 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      }
+    )
     .then(() => {
       console.log("Connected to MongoDB");
     })
