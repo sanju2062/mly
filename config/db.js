c94efbfae9be4448f23b347b61a6a6f505729950
sanju2062 <sk079997@gmail.com>
2024-08-23 03:05:23 +0530

    database debugging

--- config/db.js
+++ config/db.js
@@ -3,13 +3,17 @@ const config = require("config");
 const db = process.env.mongoURI || config.get("mongoURI");
 
 const connectDB = async () => {
+  console.log(process.env.mongoURI);
   mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Increase timeout to 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    })
+    .connect(
+      "mongodb+srv://SanjuDeveloper:Mausam123@sanjudeveloper.rm0ud.mongodb.net/ABCD?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Increase timeout to 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      }
+    )
     .then(() => {
       console.log("Connected to MongoDB");
     })
